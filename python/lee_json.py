05afa74383265c5795104f08a23003467fee6109
Marcelo suazo <marcelo.suazo@gmail.com>
2022-04-19 16:59:30 -0400

    Se parametriza el codigo

--- python/lee_json.py
+++ python/lee_json.py
@@ -1,21 +1,46 @@
 import pandas as pd
-#AUTOTT:::
+import numpy as np
+import json
 
-
-#Marcelo estuvo aqui
+from sklearn.cluster import KMeans
 
 def leeJSON(path_, file_):
     completePath = f"{path_}{file_}"
     df = pd.read_json(completePath)
-    print(df.to_string()) 
+    return df 
 
-def clasificaPor(path_, file_,type_):
-    type_ # ese el campo por el que se nos va a ocurrir clasificator
+def clasificaPor(cdato, argcalificador,ncluster):
+    # cdato es el set de datos original debe contener el campo id_interno
+    # argcalificador # ese el conjunto de campos por el que se nos van a clasificar los grupos, deben ser valores numericos
+    # ncluster  la cantidad de cluster a utilizar
     #ejemplos:
+    ccluster = np.array(cdato[argcalificador])
+    kmeans = KMeans(n_clusters=ncluster).fit(ccluster)
+    labeles = kmeans.labels_
+    df_labels = pd.DataFrame(labeles, columns=['Fila'])
+    Salida = pd.concat([cdato["id_interno"], df_labels], axis=1)
+    return Salida
 
 
 
-leeJSON("","test_json.json")
+carpeta ="C:\\documentos\\2022\\Rene\\TEST_CLASIFICATOR\\python\\"
+Archivo ="test_json.json"
+Datos = leeJSON(carpeta,Archivo)
+
+ncluster  =4
+camposclasificadores = ["y1"]
+csalida =clasificaPor(Datos,camposclasificadores,ncluster)
+
+print(csalida)
+js = csalida.to_json(orient = 'records')
+
+Salida="csalida.json"
+rutasalida= carpeta + Salida
+with open(rutasalida, 'w') as file:
+    json.dump(js, file)
+
+
+
 
 
 
